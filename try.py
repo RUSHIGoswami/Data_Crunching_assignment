64880c89c0d337740491a73656f3a47180739100
RUSHIGoswami <goswamirushi39@gmail.com>
2023-02-01 16:01:23 +0530

    After this commit code will read files with small chunks.

--- try.py
+++ try.py
@@ -1,15 +1,22 @@
 import pandas as pd
-
+import time
 
 '''Here I will iteratively chunk the file in small amounts and concat them in iterations, but due to less time I am having i am uploading this much only but will complete this for sure.'''
-
-tsv1 = pd.read_csv("user_email_hash.1m.tsv", sep='\t')
-tsv2 = pd.read_csv("ip_1m.tsv", sep='\t')
+start = time.time()
+tsv1 = pd.read_csv("user_email_hash.1m.tsv", sep='\t', chunksize=100000)
+tsv2 = pd.read_csv("ip_1m.tsv", sep='\t', chunksize=100000)
 
 columns = ['email', 'plain_pass']
-tsv3 = pd.read_csv("plain_32m.tsv", header=None, names=columns, sep='\t', )
+tsv3 = pd.read_csv("plain_32m.tsv", header=None, names=columns, sep='\t', chunksize=100000)
+end = time.time()
 
-opnew = pd.merge(tsv1, tsv3, how='left', on='email')
-optsv = pd.merge(opnew, tsv2, how='left', on='id')
+user = pd.concat(tsv1)
+ip = pd.concat(tsv2)
+plain = pd.concat(tsv3)
 
-print(optsv)
+startMerge = time.time()
+'''Here I am using left outer join in order to write only those plain password and IPs which are available on user_email_hash.1m.tsv file as we would always have user_id and related user_name with email  '''
+opnew = pd.merge(user, plain, how='left', on='email')
+optsv = pd.merge(opnew, ip, how='left', on='id')
+endMerge = time.time()
+optsv.to_csv('output.tsv', sep='\t')
